ecd1d26c0b497f6b657b9c9544fe5ba2fe0213dd
jasonstevens13 <jasonstevens13@yahoo.com>
2020-04-04 10:42:32 -0400

    api-check-1

---
+++ assets/script.js
@@ -0,0 +1,25 @@
+// $(document).ready(function () {
+
+//     var queryURL = "https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata"
+
+//     $.ajax({
+//         url: queryURL,
+//         method: "GET"
+//     }).then(function (response) {
+
+//         console.log(response);
+
+//         var recipeImg = response.meals.0.strMealThumb.val().toString();
+
+//         $("#recipeImg").attr("src", recipeImg);
+
+
+
+
+
+
+//     });
+
+
+
+// });
