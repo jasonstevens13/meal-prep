6a872309d70d619b7fec0af88e8cea731d09e849
jasonstevens13 <jasonstevens13@yahoo.com>
2020-04-04 11:05:20 -0400

    test change

--- assets/script.js
+++ assets/script.js
@@ -1,25 +1,26 @@
-// $(document).ready(function () {
+$(document).ready(function () {
 
-//     var queryURL = "https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata"
+    var queryURL = "https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata"
 
-//     $.ajax({
-//         url: queryURL,
-//         method: "GET"
-//     }).then(function (response) {
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
 
-//         console.log(response);
+        console.log(response);
 
-//         var recipeImg = response.meals.0.strMealThumb.val().toString();
+        var recipeImg = response.meals.0.strMealThumb.val().toString();
 
-//         $("#recipeImg").attr("src", recipeImg);
-
-
+        $("#recipeImg").attr("src", recipeImg);
 
 
 
 
-//     });
 
 
+    });
 
-// });
+    console.log("hello this is a test");
+
+
+});
