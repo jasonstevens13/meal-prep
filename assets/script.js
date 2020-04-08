de94a8d06b5f38768e1d68d3b9496b1de3c8dbd4
jasonstevens13 <jasonstevens13@yahoo.com>
2020-04-08 00:17:31 -0400

    api for entree checked, paint dom, some html mods for ease

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,59 @@
 $(document).ready(function () {
 
-    var queryURL = "https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata"
 
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function (response) {
 
-        console.log(response);
+    $("#searchDinnerBtn").on("click", function (event) {
+        event.preventDefault();
 
-        var recipeImg = response.meals.0.strMealThumb.val().toString();
 
-        $("#recipeImg").attr("src", recipeImg);
+        var dinnerIngredient = $("#searchDinnerInput").val();
+        console.log(dinnerIngredient);
+        var queryURL = "https://www.themealdb.com/api/json/v1/1/filter.php?i=" + dinnerIngredient;
 
+
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+
+            $("#dinnerResults").empty();
+            var responseArray = response.meals;
+
+            for (let i = 0; i < responseArray.length; i++) {
+
+                var recipeMealTitle = responseArray[i].strMeal;
+                var recipeImg = responseArray[i].strMealThumb;
+                var recipeId = responseArray[i].idMeal;
+
+                console.log("___________");
+                console.log(recipeMealTitle);
+                console.log(recipeImg);
+                console.log(recipeId);
+
+                // vars for updating search list in DOM in bulma css style
+                var article = $("<article>", { class: "tile is-child box", id: recipeId });
+                var p = $("<p>", { class: "title", text: recipeMealTitle });
+                var figure = $("<figure>", { class: "image is-4by3" });
+                var img = $("<img>", { src: recipeImg });
+                var br = $("<br>");
+
+                // updates search list here
+                $("#dinnerResults").append(article);
+                $(article).append(p);
+                $(article).append(figure);
+                $(figure).append(img);
+                $("#dinnerResults").append(br);
+
+
+            }
+
+            $("#searchDinnerInput").val('');
+
+            document.readyState;
+
+
+
+        });
 
 
 
@@ -20,7 +61,7 @@ $(document).ready(function () {
 
     });
 
-    console.log("hello this is a test");
+
 
 
 });
